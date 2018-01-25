39a8705e8f6fa9a70a66f1df8afa03fe94c83b29
clément chenebault <clem.chenebault@gmail.com>
2018-01-25 20:16:56 +0000

    clean xp and add plane batch

--- particles-nicoptere/src/js/scenes/ParticlesScene.js
+++ particles-nicoptere/src/js/scenes/ParticlesScene.js
@@ -26,8 +26,7 @@ export default class ParticlesScene
 		this.camera.perspective(45, POLY.GL.aspectRatio, 0.1, 100.0)
 		this.orbitalControl = new POLY.control.OrbitalControl(this.camera.matrix);
 
-		this._matrix = mat4.create();
-		this.normalMatrix = mat3.create();
+		POLY.GL.setCamera(this.camera); // set camera to automatically update shader matrices
 
 	    this.gl = POLY.gl;
 
@@ -39,50 +38,15 @@ export default class ParticlesScene
 		let width = 256;
 		let height = 256;
 		this.fbo = new POLY.FrameBuffer(width, height);
-
-        // let len = w * h * 4;
-        // let data = new Float32Array( len );
-
-        // while( len-- )data[len] = ( Math.random() );
-		console.log(POLY.loadedResources[window.ASSET_URL + 'image/noise.jpg'].data);
 		let data = this.getImage(POLY.loadedResources[window.ASSET_URL + 'image/noise.jpg'].data, width, height, 100);
-
-		this.texture = new POLY.Texture(window.ASSET_URL + 'image/noise.jpg');
-		console.log(data);
-
         this.dataTexture = new POLY.DataTexture(data, width, height, this.gl.RGB);
 
-
-        this.simulationProgram = new POLY.Program(simulation_vs, simulation_fs, {
-        	projectionMatrix: {
-        		value: this.camera.projectionMatrix,
-        		type: 'mat4'
-        	},
-	        modelMatrix: {
-	        	value: this.modelMatrix,
-	        	type: 'mat4'
-	        },
-	        viewMatrix: {
-	        	value: this.camera.matrix,
-	        	type: 'mat4'
-	        }
-        });
+        this.simulationProgram = new POLY.Program(simulation_vs, simulation_fs);
         this.geomSim = new POLY.geometry.Quad(this.simulationProgram, null, stateRendering);
 
         this.renderingProgram = new POLY.Program(render_vs, render_fs, {
-        	projectionMatrix: {
-        		value: this.camera.projectionMatrix,
-        		type: 'mat4'
-        	},
-	        modelMatrix: {
-	        	value: this.modelMatrix,
-	        	type: 'mat4'
-	        },
-	        viewMatrix: {
-	        	value: this.camera.matrix,
-	        	type: 'mat4'
-	        },
-	        pointSize: {
+	        pointSize:
+			{
 	        	value: 1,
 	        	type: 'float'
 	        }
@@ -101,6 +65,8 @@ export default class ParticlesScene
 
         this.geomRendering = new POLY.geometry.Mesh(this.renderingProgram, stateRendering, POLY.gl.POINTS);
         this.geomRendering.addPosition(vertices);
+
+		this.planes = new POLY.helpers.BatchPlanes();
 
 	}
 
@@ -126,12 +92,11 @@ export default class ParticlesScene
 		    var i3 = i * 3;
 		    var i4 = i * 4;
 		    data[ i3 ]      = ( ( i % width ) / width  -.5 ) * width;
-		    data[ i3 ]      /= 255;
+		    data[ i3 ]      /= 100;
 		    data[ i3 + 1 ]  = ( iData[i4] / 0xFF * 0.299 +iData[i4+1]/0xFF * 0.587 + iData[i4+2] / 0xFF * 0.114 ) * elevation;
-		    data[ i3 + 1 ]  /= 255;
-		    // data[ i3 + 1 ]  = 1;
+		    data[ i3 + 1 ]  /= 100;
 		    data[ i3 + 2 ]  = ( ( i / width ) / height -.5 ) * height;
-		    data[ i3 + 2 ]  /= 255;
+		    data[ i3 + 2 ]  /= 100;
 		}
 
 		return data;
@@ -142,9 +107,6 @@ export default class ParticlesScene
 		this.tick++;
 
 		this.orbitalControl.update();
-
-	  	mat3.fromMat4(this.normalMatrix, this._matrix);
-		mat3.transpose(this.normalMatrix, this.normalMatrix);
 
 		// this.fbo.bind();
 		// this.simulationProgram.bind();
@@ -159,14 +121,13 @@ export default class ParticlesScene
 		// POLY.gl.enable(POLY.gl.BLEND);
 		// POLY.gl.blendEquation(POLY.gl.FUNC_ADD);
 		// POLY.gl.blendFunc(POLY.gl.SRC_ALPHA, POLY.gl.ONE);
+		this.planes.draw();
 
 		this.renderingProgram.bind();
-		this.fbo.textures[0].bind(0);
+		// this.fbo.textures[0].bind(0);
 		this.dataTexture.bind(0);
-		this.renderingProgram.uniforms.projectionMatrix = this.camera.projectionMatrix;
-	    this.renderingProgram.uniforms.viewMatrix = this.camera.matrix;
-	    this.renderingProgram.uniforms.modelMatrix = this.geomRendering._matrix;
 	    POLY.GL.draw(this.geomRendering);
+
 
 	    this.fbo.clear();
 	}
