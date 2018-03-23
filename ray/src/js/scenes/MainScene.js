fac1e856c04c25fcf4052942ca72f5f72cdbd65f
clément chenebault <clem.chenebault@gmail.com>
2018-03-23 16:41:29 +0000

    ray working

--- ray/src/js/scenes/MainScene.js
+++ ray/src/js/scenes/MainScene.js
@@ -32,6 +32,9 @@ export default class MainScene
 		this.sphere2 = new POLY.geometry.Sphere(this.program);
 		this.sphere2.scale.set(.2);
 
+		this.sphereIntersection = new POLY.geometry.Cube(this.program);
+		this.sphereIntersection.scale.set(.2);
+
 		this.sphereMouse = new POLY.geometry.Sphere(this.program);
 
 		document.addEventListener('mousedown', this.onDown.bind(this));
@@ -49,8 +52,8 @@ export default class MainScene
 		])
 
 
-		this.planeP1 = [0,0,.5]
-		this.planeP2 = [1,1,1]
+		this.planeP1 = [0,0,0]
+		this.planeP2 = [1,1,0]
 		this.planeP3 = [0, -1,0]
 
 
@@ -75,12 +78,13 @@ export default class MainScene
 
 		this.geometries = [
 			this.viewRay,
-			c1,
-			c2,
-			c3,
+			// c1,
+			// c2,
+			// c3,
 			this.sphere,
+			this.sphere2,
+			this.sphereIntersection,
 			shape
-			// this.sphere2
 		];
 		this.rayCamera = new POLY.core.Ray();
 
@@ -143,6 +147,7 @@ export default class MainScene
 		let v1 = [p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2]]
 		let v2 = [p3[0] - p2[0], p3[1] - p2[1], p3[2] - p2[2]]
 
+		// https://www.youtube.com/watch?v=0qYJfKG-3l8
 		// cross product
 		// |i     j     k
 		// |v1[0] v1[1] v1[2]
@@ -152,24 +157,35 @@ export default class MainScene
 		// | v2[1] v2[2]| i     |v2[0]v2[2]| j    |v2[0] v2[1]|k
 
 		// (v1[1] * v2[2] + v1[2] * v2[1]) * i - (v1[0] * v2[2] + v1[2] * v2[0]) * j + (v1[0] * v2[1] + v1[1] * v2[0]) * k
-		let dotProduct;
 		let abc = [(v1[1] * v2[2] + v1[2] * v2[1]), - (v1[0] * v2[2] + v1[2] * v2[0]) ,  -(v1[0] * v2[1] + v1[1] * v2[0])]
 
-		abc = vec3.create();
-		vec3.cross(abc, v1, v2)
+		// let abc = vec3.create();
+		// vec3.cross(abc, v1, v2)
+		// abc[2] *= -1;
+		// console.log(abc);
 
-		console.log(abc);
-
-		this.sphere.position.set(abc[0], abc[1], abc[2]);
+		// this.sphere.position.set(abc[0], abc[1], abc[2]);
 
 
 		// plane equation
 		// abc[0] * (x - x0) + abc[1] * (y-y0) + abc[2](z- z0) = 0
+		// abc[0] * x - abc[0] * x0 + abc[1] * y - abc[1] * y0 + abc[2] * z - abc[2] * z0 = 0
+		// abc[0] * x  +  abc[1] * y + abc[2] * z = abc[0] * x0 + abc[1] * y0 + abc[2] * z0;
 
 
 		// line equation
-		// r(t) = < pt1[0], pt1[1], pt1[2] > + t <pt2[0], pt2[1], pt2[2]>
-		// 1*) r(t) = pt1[0] + t * pt2[0] , t * pt2[1] + pt1[1] , t * pt2[2] + pt1[2];
+		// r(t) = < pt1[0], pt1[1], pt1[2] > + t <pt2[0] - pt1[0], pt2[1] - pt1[1], pt2[2] - pt1[2]>
+		// 1*) r(t) = pt1[0] + t * (pt2[0] - pt1[0]) , t * (pt2[1] - pt1[1]) + pt1[1] , t * (pt2[2] - pt1[2]) + pt1[2];
+
+		// abc[0] * (pt1[0] + t * (pt2[0] - pt1[0]))  +  abc[1] * (t * (pt2[1] - pt1[1]) + pt1[1]) + abc[2] * (t * (pt2[2] - pt1[2]) + pt1[2]) = abc[0] * x0 + abc[1] * y0 + abc[2] * z0;
+		// abc[0] * pt1[0] + abc[0] * t * (pt2[0] - pt1[0]) + abc[1] * pt1[1] + abc[1] * t * (pt2[1] - pt1[1]) + abc[2] * pt1[2] + abc[2] * t * (pt2[2] - pt1[2]) = abc[0] * x0 + abc[1] * y0 + abc[2] * z0;
+		//  abc[0] * t * (pt2[0] - pt1[0]) + abc[1] * t * (pt2[1] - pt1[1]) + abc[2] * t * (pt2[2] - pt1[2]) = abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0]- abc[1] * pt1[1] - abc[2] * pt1[2];
+		// t * (abc[0] * (pt2[0] - pt1[0]) + abc[1] * (pt2[1] - pt1[1]) + abc[2] * (pt2[2] - pt1[2])) = abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0]- abc[1] * pt1[1] - abc[2] * pt1[2];
+		// t = (abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0]- abc[1] * pt1[1] - abc[2] * pt1[2]) / (abc[0] * (pt2[0] - pt1[0]) + abc[1] * (pt2[1] - pt1[1]) + abc[2] * (pt2[2] - pt1[2]));
+
+		// abc[0] * t * pt2[0] + abc[1] * t * pt2[1] + abc[2] * t * pt2[2] = abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0] - abc[1] * pt1[1] - abc[2] * pt1[2]
+		// t * (abc[0] * pt2[0] + abc[1] * pt2[1] + abc[2] * pt2[2]) = abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0] - abc[1] * pt1[1] - abc[2] * pt1[2]
+		// t = (abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0] - abc[1] * pt1[1] - abc[2] * pt1[2]) / (abc[0] * pt2[0] + abc[1] * pt2[1] + abc[2] * pt2[2])
 
 		// plane equation:
 		// 0 = abc[0] * (p1[0] + t * p2[0] - x0) + abc[1] * (t * p2[1] + p1[1]-y0) + abc[2](t * p2[2] + p1[2]- z0)
@@ -183,19 +199,27 @@ export default class MainScene
 
 		// 0 = t * (abc[0] * p2[0] + abc[1] * p2[1] + abc[2] * p2[2]) + abc[0] * p1[0] + abc[0]*x0  + abc[1]*p1[1] - abc[0]*y0 + abc[2]*p1[2] - abc[2] * z0
 		// t = -(abc[0] * p1[0] + abc[0]*x0  + abc[1]*p1[1] - abc[0]*y0 + abc[2]*p1[2] - abc[2] * z0) / (abc[0] * p2[0] + abc[1] * p2[1] + abc[2] * p2[2])
-		let t = -(abc[0] * p1[0] + abc[0]*x0  + abc[1]*p1[1] - abc[0]*y0 + abc[2]*p1[2] - abc[2] * z0) / (abc[0] * p2[0] + abc[1] * p2[1] + abc[2] * p2[2]);
+		// let t = -(abc[0] * p1[0] + abc[0]*x0  + abc[1]*p1[1] - abc[0]*y0 + abc[2]*p1[2] - abc[2] * z0) / (abc[0] * p2[0] + abc[1] * p2[1] + abc[2] * p2[2]);
+		let t = (abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0]- abc[1] * pt1[1] - abc[2] * pt1[2]) / (abc[0] * (pt2[0] - pt1[0]) + abc[1] * (pt2[1] - pt1[1]) + abc[2] * (pt2[2] - pt1[2]));
 
 		// if(!t) t = 0;
 		console.log('t', t);
+		console.log((abc[0] * x0 + abc[1] * y0 + abc[2] * z0 - abc[0] * pt1[0]- abc[1] * pt1[1] - abc[2] * pt1[2]), (abc[0] * (pt2[0] - pt1[0]) + abc[1] * (pt2[1] - pt1[1]) + abc[2] * (pt2[2] - pt1[2])));
 
 		// so when we replace the line above (1*))
 		// = pt1[0] + t * pt2[0] , t * pt2[1] + pt1[1] , t * pt2[2] + pt1[2];
-		let newx = t * pt1[0] + pt2[0];
-		let newy = t * pt1[1] + pt2[1];
-		let newz = t * pt1[2] + pt2[2];
+		let newx = t * (pt2[0] - pt1[0]) + pt1[0];
+		let newy = t * (pt2[1] - pt1[1]) + pt1[1];
+		let newz = t * (pt2[2] - pt1[2]) + pt1[2];
 
+		// let l = pt1[0] + t * (pt2[0] - pt1[0]) , t * (pt2[1] - pt1[1]) + pt1[1] , t * (pt2[2] - pt1[2]) + pt1[2]
 
 		// this.sphere.position.set(pt1[0], pt1[1], pt1[2]);
+		this.sphere.position.set(pt2[0], pt2[1], pt2[2]);
+		this.sphere2.position.set(pt1[0], pt1[1], pt1[2]);
+
+		this.sphereIntersection.position.set(newx, newy, newz);
+		// this.sphere.position.set(newx, newy, newz);
 		// this.sphere2.position.set(pt2[0], pt2[1], pt2[2]);
 
 		console.log('here');
